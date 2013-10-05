fdd148cbba17cdbeec3ca27c14e48e7f907d3e00
Alex Choi <rlrrlrll4@gmail.com>
2013-10-05 13:38:17 -0500

    first arduino commit

---
+++ are_do_we_know/window.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>
+
+Servo left; //pin 5
+Servo right; //pin 6
+int ele_imp = 4; //input from imp
+int up = 0; //window up = 1
+
+void setup(){
+  left.attach(5);
+  right.attach(6);
+  pinMode(ele_imp, INPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  up = digitalRead(ele_imp);
+  if(up){
+    left.write(90);
+    right.write(90);
+  }else{
+    left.write(0);
+    right.write(0);
+  }
+}
