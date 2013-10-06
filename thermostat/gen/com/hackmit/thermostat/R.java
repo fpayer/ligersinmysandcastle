01c4b7bebb7d9c37e661314ae301e43fe9563931
Frank Cangialosi <fcangialosi@studyblue.com>
2013-10-06 03:16:58 -0400

    continuously updating thermostat temp works

--- thermostat/gen/com/hackmit/thermostat/R.java
+++ thermostat/gen/com/hackmit/thermostat/R.java
@@ -24,11 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int desired_temp_text=0x7f080003;
-        public static final int fullscreen_content=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int current_temp=0x7f080000;
+        public static final int desired_temp_text=0x7f080004;
+        public static final int fullscreen_content=0x7f080002;
         public static final int fullscreen_content_controls=0x7f080001;
-        public static final int seekBar1=0x7f080002;
+        public static final int seekBar1=0x7f080003;
     }
     public static final class layout {
         public static final int activity_thermostat_main=0x7f030000;
--- thermostat/src/com/hackmit/thermostat/ThermostatMain.java
+++ thermostat/src/com/hackmit/thermostat/ThermostatMain.java
@@ -1,6 +1,27 @@
 package com.hackmit.thermostat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.view.Menu;
 import android.widget.SeekBar;
@@ -8,6 +29,10 @@ import android.widget.TextView;
 
 public class ThermostatMain extends Activity implements SeekBar.OnSeekBarChangeListener {
 	private TextView desiredTemp;
+	private TextView currentTemp;
+    // Need handler for callbacks to the UI thread
+    final Handler mHandler = new Handler();
+    private String setTemp;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +41,11 @@ public class ThermostatMain extends Activity implements SeekBar.OnSeekBarChangeL
 		
 		SeekBar bar = (SeekBar) findViewById(R.id.seekBar1);
 		desiredTemp = (TextView) findViewById(R.id.desired_temp_text);
-		
+		currentTemp = (TextView) findViewById(R.id.current_temp);
 		bar.setOnSeekBarChangeListener(this);
 		
+		setTemp = "0";
+		mHandler.post(mUpdateResults);
 	}
 
 	@Override
@@ -46,5 +73,76 @@ public class ThermostatMain extends Activity implements SeekBar.OnSeekBarChangeL
         // TODO Auto-generated method stub
         
     }
+    
+
+    
+    // Create runnable for posting
+    final Runnable mUpdateResults = new Runnable() {
+        public void run() {
+        	startCheckingTemperature();
+            
+        	updateResultsInUi();
+            mHandler.postDelayed(mUpdateResults, 1000);
+        }
+    };
+    
+    private void updateResultsInUi(){
+    	currentTemp.setText(setTemp +" °F");
+    }
+    
+   
+    
+    protected void startCheckingTemperature() {
+
+        // Fire off a thread to do some work that we shouldn't do directly in the UI thread
+        Thread t = new Thread() {
+            public void run() {
+            	HttpClient client = new DefaultHttpClient();
+    			HttpGet get = new HttpGet("http://162.243.27.156/getInsideTemp");
+    			try {
+    		        // Execute HTTP Post Request
+    		        HttpResponse response = client.execute(get);
+    		        String resp = getStringFromInputStream(response.getEntity().getContent());
+    		        setTemp = resp;
+    		    } catch (ClientProtocolException e) {
+    		        System.out.println("CLIENT PROTOCOL EXCEPTION: " + e);
+    		    } catch (IOException e) {
+    		        System.out.println("IO EXCEPTION: " + e);
+    		    }
+    			
+            }
+        };
+        t.start();
+    }
+
+    
+    private static String getStringFromInputStream(InputStream is) {
+    	 
+		BufferedReader br = null;
+		StringBuilder sb = new StringBuilder();
+ 
+		String line;
+		try {
+ 
+			br = new BufferedReader(new InputStreamReader(is));
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+			}
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+ 
+		return sb.toString();
+ 
+	}
 
 }
