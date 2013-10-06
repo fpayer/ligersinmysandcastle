91e01da4d230091e4115654ea7f22ca5c081ba7e
Alex Choi <rlrrlrll4@gmail.com>
2013-10-06 07:09:24 -0500

    arduino single servo

--- are_do_we_know/are_do_we_know.ino
+++ are_do_we_know/are_do_we_know.ino
@@ -1,13 +1,13 @@
 #include <Servo.h>
 
 Servo left; //pin 5
-Servo right; //pin 6
+//Servo right; //pin 6
 int ele_imp = 4; //input from imp
 int up = 0; //window up = 1
 
 void setup(){
   left.attach(5);
-  right.attach(6);
+  //right.attach(6);
   pinMode(ele_imp, INPUT);
   Serial.begin(9600);
 }
@@ -16,9 +16,9 @@ void loop(){
   up = digitalRead(ele_imp);
   if(up){
     left.write(90);
-    right.write(90);
+    //right.write(90);
   }else{
     left.write(1);
-    right.write(1);
+    //right.write(1);
   }
 }
